f57c9095ae458b8c5277f56718a096c34bdfd0c1
Anderson Briglia <anderson.briglia@openbossa.org>
2009-04-24 22:57:36 -0400

    Adding test-canola to source tree
    test-canola.py is the automated test for Canola
    
    Signed-off-by: Anderson Briglia <anderson.briglia@openbossa.org>

---
+++ utilities/test-canola.py
@@ -0,0 +1,130 @@
+#!/usr/bin.python
+
+import time
+from os import system
+
+
+BT_Y = 222 # Since they are in horizontal, y is the same
+BT_OFFSET = 150 # Position offset
+
+'''
+Positions for main buttons when they are in even layout
+'''
+EV_FIRST_BT_X = 170 # X position for first bt
+AUDIO_BT = 0
+PHOTOS_BT = 1
+VIDEOS_BT = 2
+SETT_BT = 3
+MY_PHOTOS_BT = 1
+MY_VIDEOS_BT = 1
+
+'''
+Positions for buttons when they are in uneven layout
+'''
+UNEV_FIRST_BT_X = 248
+MY_MUSIC_BT = 0
+PODCASTS_BT = 1
+RADIO_BT = 2
+
+class TestCanola:
+
+    def __init__(self):
+        self.__steps = []
+
+    def click(self, pos, sleep):
+        cmd = ("xte -x :0.0 'mousemove " + str(pos[0]) + " " + str(pos[1]) + "' 'mouseclick 1'", sleep)
+        self.add_step(cmd)
+
+    def click_back(self):
+        '''
+        Click back button.
+        '''
+        self.click((45, 455), 3)
+
+    def click_bt(self, button, first_bt_pos):
+        '''
+        Click buttons presented in main window.
+        '''
+        x = (button * BT_OFFSET) + first_bt_pos
+        self.click((x, BT_Y), 3)
+
+    def click_list(self, pos):
+        '''
+        Click in a list position
+        Since just seven elements are showed each time, pos must be
+        checked against these values. This method does not implement
+        list rolling.
+        '''
+        if (pos >= 0) and (pos < 7):
+            self.click((160, 68 + (pos * 63)), 3)
+
+    def play_pause_music(self):
+        self.click((759, 227), 2);
+
+    def photo_click(self, photo):
+        '''
+        photo = photo number, starting in 0.
+        '''
+        x1 = 58
+        y1 = 118
+        x_offset = 146
+        y_offset = 115
+        if photo < 5:
+            self.click(x1 + (photo * x_offset), y1)
+        elif (photo >= 5) and (photo < 10):
+            self.click((x1 + ((photo - 6) * x_offset), y1 + y_offset), 4)
+        elif (photo >= 10) and (photo < 15):
+            self.click((x1 + ((photo - 11) * x_offset), y1 + (2 * y_offset)), 4)
+
+    def click_yes(self):
+        self.click((508, 297), 2)
+
+    def add_delay(self, delay):
+        self.add_step(('', delay))
+
+    def add_step(self, cmd):
+        self.__steps.append(cmd)
+
+    def play_steps(self):
+        '''
+        Run each command stored in self.__steps.
+        Uses time.sleep() to make an interval between each command.
+        '''
+        for cmd in self.__steps:
+            #print cmd
+            if cmd[0] != '':
+                system(cmd[0])
+            time.sleep(cmd[1])
+
+    def begin_test(self):
+        # Playing a music
+        self.click_bt(AUDIO_BT, EV_FIRST_BT_X)
+        self.click_bt(MY_MUSIC_BT, UNEV_FIRST_BT_X)
+        self.click_list(0)
+        self.click_list(1)
+        self.add_delay(10)
+        self.play_pause_music()
+        self.click_back()
+        self.click_back()
+        self.click_back()
+        self.click_back()
+        #Opening a photo
+        self.click_bt(PHOTOS_BT, EV_FIRST_BT_X)
+        self.click_bt(MY_PHOTOS_BT, EV_FIRST_BT_X)
+        self.click_list(0)
+        self.photo_click(6)
+        self.click_back()
+        self.click_back()
+        self.click_back()
+        self.click_back()
+        # End
+        self.click_back()
+        self.click_yes()
+        self.play_steps()
+
+if __name__ == '__main__':
+    print 'Initializing tests...'
+    test = TestCanola()
+    test.begin_test()
+    print 'Test finished...'
+
