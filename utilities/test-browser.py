11762e2eb11953bd51f6bbf4f86cd1a433d3fc43
Anderson Briglia <anderson.briglia@openbossa.org>
2009-04-28 20:36:53 -0400

    Adding GPL header
    - Added GPL header into python test scripts
    
    Signed-off-by: Anderson Briglia <anderson.briglia@gmail.com>

--- utilities/test-browser.py
+++ utilities/test-browser.py
@@ -1,4 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin.python
+#
+# Copyright (c) 2009 - Anderson Farias Briglia
+#                      <anderson.briglia@gmail.com>
+#
+# This file is part of carman-python.
+#
+# carman-python is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# carman-python is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import time
 from os import system
