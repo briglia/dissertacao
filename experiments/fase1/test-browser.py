3096fcb516c8b63419fec884340724d48ac8f51b
Anderson Briglia <anderson.briglia@openbossa.org>
2009-09-17 02:20:16 -0400

    Conserto nos arquivos de teste
    Conserto no cabecalho do arquivo. Coisa pouca.
    
    Signed-off-by: Anderson Briglia <anderson.briglia@openbossa.org>

--- experiments/fase1/test-browser.py
+++ experiments/fase1/test-browser.py
@@ -5,12 +5,12 @@
 #
 # This file is part of carman-python.
 #
-# carman-python is free software: you can redistribute it and/or modify
+# test-browser is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# carman-python is distributed in the hope that it will be useful,
+# test-browser is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
